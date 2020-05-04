8d292519b1967d8f690d50d584e993546e234b6d
smallconan <zcb1693749255@outlook.com>
2020-05-04 21:32:44 +0800

    Site updated: 2020-05-04 21:32:44

--- js/customer.js
+++ js/customer.js
@@ -1,7 +1,5 @@
 $(function () {
-  $(".footer-inner").append("<div>" +
-    "<span><a href='http://www.beian.miit.gov.cn/'>豫ICP备19041240号</a></span>" +
-    "</div>" +
-    "<div> Powered By Hexo Theme Next</div>");
+  $(".footer-inner").append("<span><a href='http://www.beian.miit.gov.cn/'>豫ICP备19041240号</a></span>" +
+    "<span>  | Powered By Hexo Theme Next</span>");
 });
 
